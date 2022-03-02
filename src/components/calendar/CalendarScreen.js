b96efafd4ed37e5558c0c814ae7e25eac33ef1e1
AlexFabra <alfaseart@gmail.com>
2022-03-02 11:45:20 +0100

    pagina de login y componente sidebar creados, bootstrap y fontawesome implementados

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,7 +1,10 @@
+import { NavBar } from '../ui/NavBar'
 import React from 'react'
 
 export const CalendarScreen = () => {
   return (
-    <div>CalendarScreen</div>
+    <div>
+      <NavBar/>
+    </div>
   )
 }
