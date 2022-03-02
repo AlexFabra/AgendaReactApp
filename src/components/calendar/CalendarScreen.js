707691ca07c5592d79a6a810e1060bbc4d92d1d3
AlexFabra <alfaseart@gmail.com>
2022-03-02 12:09:44 +0100

    importado react-big-calendar e implementado sus estilos

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,33 @@
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+
 import { NavBar } from '../ui/NavBar'
 import React from 'react'
+import moment from 'moment'
+
+//configuramos el moment:
+const localizer = momentLocalizer(moment);
+const events = [{
+  title: 'El dia d',
+  start: moment().toDate(),
+  end: moment().add(2, 'hours').toDate(),
+  bgcolor: '#fafafa'
+}]
 
 export const CalendarScreen = () => {
   return (
     <div>
-      <NavBar/>
+      <NavBar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+
     </div>
   )
 }
--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export const NavBar = () => {
     return (
-        <div className='navbar navbar.dark bg-dark mb-4'>
-            <span className='navBar-brand'>
+        <div className='navbar navbar-light bg-light mb-4'>
+            <span className='navbar-brand'>
                 Pep
             </span>
             <button className="btn btn-outline-danger">
+                <i className='fas fa-sign-out-alt'></i>
                 <span>Salir</span>
             </button>
         </div>
