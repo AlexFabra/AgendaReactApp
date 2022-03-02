f9780f80da196e832d206bdbb47615880f804bee
AlexFabra <alfaseart@gmail.com>
2022-03-02 12:23:16 +0100

    añadido estilo de evento

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,14 @@
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'moment/locale/es'
 
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 
 import { NavBar } from '../ui/NavBar'
 import React from 'react'
+import { messages } from '../../helpers/calendar-messages-es'
 import moment from 'moment'
+
+moment.locale('es');
 
 //configuramos el moment:
 const localizer = momentLocalizer(moment);
@@ -12,10 +16,25 @@ const events = [{
   title: 'El dia d',
   start: moment().toDate(),
   end: moment().add(2, 'hours').toDate(),
-  bgcolor: '#fafafa'
+  bgcolor: '#fafafa',
+  notes:'es el momento'
 }]
 
 export const CalendarScreen = () => {
+
+//lo que regrese esta constante es el estilo que le va a aplicar 
+//al evento.
+const eventStyleGetter = (event,start,end,isSelected)=> {
+  const style = {
+    backgroundColor:'#367CF7',
+    borderRadius:'0px',
+    opacity:0.8,
+    display:'block',
+    color:'white'
+  }
+  return { style }
+}
+
   return (
     <div>
       <NavBar />
@@ -26,6 +45,8 @@ export const CalendarScreen = () => {
         startAccessor="start"
         endAccessor="end"
         style={{ height: 500 }}
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
       />
 
     </div>
