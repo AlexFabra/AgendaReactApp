94c2a2c3f16bb7e8a71b6e2a8486bb946792b13b
Alejandro Fabra <afabra@nexusgeographics.com>
2022-03-30 11:21:50 +0200

    logout funcional

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -18,6 +18,12 @@ export const authReducer = (state = initialState, action) => {
                 ...state,
                 checking: false
             }
+
+        case types.authLogout:
+            return {
+                checking: false
+            }
+            
         default: return state;
     }
 }
